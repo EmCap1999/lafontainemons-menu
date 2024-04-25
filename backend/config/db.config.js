47016bba34485e89be2aa343e334bf41db09536d
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2024-04-25 18:41:25 +0200

    bugs fixed after client demo - need to drop/restart sql db init request and frontend container on VM

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,12 +1,28 @@
+// module.exports = {
+//     HOST: process.env.HOST,
+//     DB_HOST: process.env.DB_HOST,
+//     CLIENT_PORT: process.env.CLIENT_PORT,
+//     SERVER_PORT: process.env.SERVER_PORT,
+//     USER: process.env.POSTGRES_USER,
+//     PASSWORD: process.env.POSTGRES_PASSWORD,
+//     DB: process.env.DB,
+//     dialect: process.env.DIALECT,
+
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+
+// };
+
 module.exports = {
-    HOST: process.env.HOST,
-    DB_HOST: process.env.DB_HOST,
-    CLIENT_PORT: process.env.CLIENT_PORT,
-    SERVER_PORT: process.env.SERVER_PORT,
-    USER: process.env.POSTGRES_USER,
-    PASSWORD: process.env.POSTGRES_PASSWORD,
-    DB: process.env.DB,
-    dialect: process.env.DIALECT,
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "admin",
+    DB: "lafontainemons",
+    dialect: "postgres",
 
     pool: {
         max: 5,
