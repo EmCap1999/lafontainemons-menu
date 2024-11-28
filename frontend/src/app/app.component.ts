c055fe71983e2babc6f9fbb3a79b7b740ec89222
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2024-11-28 11:19:01 +0100

    set urls in env prod file for more readable code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { faWhiskeyGlass, faGlassWaterDroplet, faBeerMugEmpty } from '@fortawesome/free-solid-svg-icons';
+import { environment } from './environments/environment.prod'
 
 @Component({
   selector: 'app-root',
@@ -15,17 +16,20 @@ export class AppComponent {
   title = 'frontend';
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
-  isMobile= true;
+  isMobile = true;
   isCollapsed = false;
   faWhiskeyGlass = faWhiskeyGlass;
   faGlassWater = faGlassWaterDroplet;
   fafaBeerMugEmpty = faBeerMugEmpty;
+  facebookUrl = environment.facebookUrl;
+  tripadvisorUrl = environment.tripadvisorUrl
+  googleUrl = environment.GoogleUrl;
 
-  constructor(private observer: BreakpointObserver) {}
+  constructor(private observer: BreakpointObserver) { }
 
   ngOnInit() {
     this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
-      if(screenSize.matches){
+      if (screenSize.matches) {
         this.isMobile = true;
       } else {
         this.isMobile = false;
@@ -33,7 +37,7 @@ export class AppComponent {
     });
   }
   toggleMenu() {
-    if(this.isMobile){
+    if (this.isMobile) {
       this.sidenav.toggle();
       // this.isCollapsed = false; // On mobile, the menu can never be collapsed
     } else {
--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://162.19.247.38:8080/'
-  };
-  
+  production: true,
+  apiUrl: 'http://162.19.247.38:8080/',
+  facebookUrl: 'https://www.facebook.com/www.lafontaine.mons.be',
+  tripadvisorUrl: 'https://fr.tripadvisor.be/Restaurant_Review-g188647-d8045222-Reviews-Restaurant_La_Fontaine-Mons_Hainaut_Province_Wallonia.html',
+  GoogleUrl: 'https://www.google.com/search?sca_esv=f04f39c9057254ed&rlz=1C1JJTC_enBE1125BE1125&sxsrf=ADLYWILQOXa5W0YoeSwUSNJ9hngXUoDR2w:1732788743983&uds=ADvngMjcH0KdF7qGWtwTBrP0nt7dYKWZLKwU1lKup9faKIbQjoowEnoeZV0t0s8U7RaYEab8Y7NZK7MmFhdvG9GJgMBkAO9UCbQUdp-t0SLd9Z_byQ7d2_YD15AtAY-airs7krELt0ZK_hBBr8gQ96lNbXsmBAqjVg&si=ACC90nzcy7sviKw0NTZoUBUzhQehr3jouizIrVSf6avWI23m1cdb3ublOaiJTtvmY-ls1P-gSxqYf0cNgAOI7CVXOUI-eX9g3WHgCDHJDdsj1sygsAIe9Yc%3D&q=La+Fontaine+Avis&sa=X&ved=2ahUKEwi-_LiM5f6JAxUxU6QEHaZfLaQQ3PALegQITBAF&biw=1368&bih=733&dpr=2'
+};
