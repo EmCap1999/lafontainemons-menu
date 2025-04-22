96feec4dcec0549735149760b95ddafef1cbd3eb
Manu Caputo <emmanuel.caputo@necko.tech>
2025-04-22 18:12:05 +0200

    refactor(server): code refactoring for server launch

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import cors from 'cors'
 import express from 'express'
 import './config/environment.js'
 import corsOptions from './config/cors.js'
+import { testDatabaseConnection } from './config/database.js'
 import { AppError, handleErrors } from './errors/app-errors.js'
 
 const app = express()
@@ -11,20 +12,19 @@ app.use(express.json())
 
 app.use(handleErrors)
 
-const startServer = () => {
+const startServer = async () => {
   const PORT = process.env.BACKEND_PORT || 3000
+  await testDatabaseConnection()
 
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}...`)
   })
+
+  if (!process.env.BACKEND_PORT) {
+    console.warn('BACKEND_PORT is not defined, defaulting to port 3000.')
+  }
 }
 
-if (!process.env.BACKEND_PORT) {
-  console.warn('Warning: BACKEND_PORT is not defined, defaulting to port 3000')
-}
-
-try {
-  startServer()
-} catch (err) {
+startServer().catch((err) => {
   throw new AppError(err.message, err.code || 500)
-}
+})
