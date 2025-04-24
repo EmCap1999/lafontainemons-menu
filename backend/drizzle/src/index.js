76f04e4389500f678a94527447ad5599c160e93a
Manu Caputo <emmanuel.caputo@necko.tech>
2025-04-24 15:33:50 +0200

    feat(db): update db and backend setup with drizzle ORM

--- backend/drizzle/src/index.js
+++ backend/drizzle/src/index.js
@@ -3,10 +3,7 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 
 export const db = drizzle({
   connection: {
-    connectionString:
-      process.env.NODE_ENV === 'production'
-        ? { rejectUnauthorized: false }
-        : false,
-    ssl: true,
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
   },
 })
