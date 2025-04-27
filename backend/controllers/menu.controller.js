1545c9a5ed3d230255c3bb2e64b6b8965b69f48e
Manu Caputo <emmanuel.caputo@necko.tech>
2025-04-27 17:10:01 +0200

    feat(drizzle): chore set up backend env

--- backend/controllers/menu.controller.js
+++ backend/controllers/menu.controller.js
@@ -21,7 +21,7 @@ export const getSectionById = async (req, res, next) => {
     const section = await menuService.getSectionById(id)
 
     if (!section) {
-      return next(new AppError(`Section with ID ${id} not found`, 404))
+      return next(new AppError(`Section ID ${id} does not exist.`, 404))
     }
 
     res.status(200).json({
@@ -51,6 +51,15 @@ export const getSubsectionsBySection = async (req, res, next) => {
     const sectionId = Number.parseInt(req.params.sectionId)
     const subsections = await menuService.getSubsectionsBySection(sectionId)
 
+    if (subsections.length === 0) {
+      return next(
+        new AppError(
+          `Sub-section with Section ID ${sectionId} does not exist.`,
+          404,
+        ),
+      )
+    }
+
     res.status(200).json({
       status: 'success',
       results: subsections.length,
@@ -79,6 +88,12 @@ export const getItemsBySection = async (req, res, next) => {
     const sectionId = Number.parseInt(req.params.sectionId)
     const items = await menuService.getItemsBySection(sectionId)
 
+    if (items.length === 0) {
+      return next(
+        new AppError(`No items founded with section ID ${sectionId}`, 404),
+      )
+    }
+
     res.status(200).json({
       status: 'success',
       results: items.length,
@@ -93,6 +108,15 @@ export const getItemsBySubsection = async (req, res, next) => {
   try {
     const subsectionId = Number.parseInt(req.params.subsectionId)
     const items = await menuService.getItemsBySubsection(subsectionId)
+
+    if (items.length === 0) {
+      return next(
+        new AppError(
+          `No items founded with sub-section ID ${subsectionId}`,
+          404,
+        ),
+      )
+    }
 
     res.status(200).json({
       status: 'success',
