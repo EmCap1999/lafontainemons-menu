d23b2976f375712e2a0fdd163373de8f40a76de1
Manu Caputo <emmanuel.caputo@necko.tech>
2025-05-16 18:40:41 +0200

    fix(app): update env variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: import.meta.env.NODE_ENV,
-  frontendUrl: import.meta.env.FRONTEND_URL,
-  apiUrl: `http://localhost:${import.meta.env.BACKEND_PORT}/`,
+  production: import.meta.env.NODE_ENV === 'production',
+  frontendUrl: import.meta.env.FRONTEND_URL || 'http://localhost:4200',
+  apiUrl: `http://localhost:${import.meta.env.BACKEND_PORT || '8080'}/api`,
 }
