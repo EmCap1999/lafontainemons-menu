adc68d39dfd167497e9be4bf65c4bd79aa5f4d2a
Manu Caputo <emmanuel.caputo@necko.tech>
2025-06-26 17:22:19 +0200

    fix(drizzle): config update

--- db/config/drizzle.config.js
+++ db/config/drizzle.config.js
@@ -1,11 +1,7 @@
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import dotenv from 'dotenv'
 import dotenvExpand from 'dotenv-expand'
 import { defineConfig } from 'drizzle-kit'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const rootDirectory = path.resolve(__dirname, '../../')
 const envPath = path.join(rootDirectory, '.env')
@@ -15,12 +11,10 @@ const myEnv = dotenv.config({ path: envPath })
 dotenvExpand.expand(myEnv)
 
 export default defineConfig({
-  out: './migrations',
+  out: './drizzle',
   schema: './schema/index.js',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
-  verbose: true,
-  strict: true,
 })
