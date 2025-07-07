f6080bbe6dda53be3227db961d57052ad102dcbe
Emmanuël Caputo <136804334+EmCap1999@users.noreply.github.com>
2025-07-07 16:13:08 +0200

    feat(frontend): add powered by footer with LinkedIn link (#36)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
+import { FooterComponent } from './components/footer/footer.component'
 import { MenuComponent } from './components/menu/menu.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
