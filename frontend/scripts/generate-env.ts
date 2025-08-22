3f1658085e9a1738a8b8cb02daf3831070a0514c
Manu Caputo <emmanuel.caputo@necko.tech>
2025-08-22 23:03:28 +0200

    fix: disable noPropertyAccessFromIndexSignature in all tsconfigs

--- frontend/scripts/generate-env.ts
+++ frontend/scripts/generate-env.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
-// @ts-nocheck
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import { fileURLToPath } from 'node:url'
