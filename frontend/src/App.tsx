887990a58e2075a0ad3eddfa206835d441da389c
Manu Caputo <emmanuel.caputo@necko.tech>
2026-07-08 18:54:45 +0200

    feat(frontend): optimize mobile layout with scrollable hero and fixed footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import { Menu } from "@/components/Menu";
 
 function App() {
 	return (
-		<div className="h-dvh flex flex-col overflow-hidden">
-			<main className="flex-1 min-h-0 flex flex-col">
+		<div className="min-h-dvh flex flex-col">
+			<main className="flex-1 pb-12 sm:pb-16">
 				<Menu />
 			</main>
 			<Footer />
